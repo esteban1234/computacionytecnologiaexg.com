02676e6bc143599163255f70a3df960edab1357e
esteban1234 <tva_libra@hotmail.com>
2018-06-12 13:44:18 -0500

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="N9Qs5ciWvo-m2ctWpDnsrCDeIfAqBMEz9cV8djylVWg" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>COMPUTACION Y TECNOLOGIA INTEGRAL EXG SA DE CV</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
